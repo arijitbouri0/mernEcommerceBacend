32a8536d8e08768b902b1d68fe586bbf01a621da
Arijit Bouri <137565275+arijitbouri0@users.noreply.github.com>
2024-07-26 20:31:44 +0530

    Update connection.js

--- connection.js
+++ connection.js
@@ -2,8 +2,6 @@ const mongoose = require("mongoose");
 mongoose.set("strictQuery", true);
 async function connectToMongoDB(url) {
   return mongoose.connect(url,{
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
   serverSelectionTimeoutMS: 30000 // Increase the timeout
 });
 }
